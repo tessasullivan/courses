6a10440381695dca4c55512c9ca703c9cd0c6cc1
Student <example@example.com>
2019-06-11 15:26:43 -0700

    Added pipe examples

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -9,7 +9,13 @@ import { Component } from '@angular/core';
     <ul>
       <li *ngFor="let course of courses">{{course}}</li>
     </ul>
-    <input [(ngModel)] = "email" (keyup.enter)="onKeyUp()"/>`
+    {{ course.title | uppercase }}<br />
+    {{ course.students | number}}<br />
+    {{ course.rating | number:'1.2-2' }}<br />
+    {{ course.price | currency:'USD':true }}<br />
+    {{ course.releaseDate | date:'shortDate' }}
+    `
+    // Two-way Binding: <input [(ngModel)] = "email" (keyup.enter)="onKeyUp()"/>`
     // Template variables: <input #email (keyup.enter)="onKeyUp(email.value)" />`
     // Looking for enter to be edited: <input (keyup.enter)="onKeyUp($event)" />`
     // Event handling basic: <button (click)="onSave($event)">Save</button>`
@@ -23,6 +29,13 @@ export class CoursesComponent {
   isActive = true;
   email="me@example.com";
 
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30254,
+    price: 190.95,
+    releaseDate: new Date(2019, 6, 11)
+  }
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
