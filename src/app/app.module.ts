adb0beb1d9702f79fd162e8d5ffecf1174a14821
Student <example@example.com>
2019-06-11 15:12:36 -0700

    Added examples of class, style, and 2-way binding in courses.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 
@@ -15,7 +15,8 @@ import { CourseComponent } from './course/course.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule 
   ],
   providers: [
     CoursesService
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,20 +1,47 @@
 import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'courses',
   template: `
     <h2>{{title}}</h2>
     <ul>
       <li *ngFor="let course of courses">{{course}}</li>
-    </ul>`
+    </ul>
+    <input [(ngModel)] = "email" (keyup.enter)="onKeyUp()"/>`
+    // Template variables: <input #email (keyup.enter)="onKeyUp(email.value)" />`
+    // Looking for enter to be edited: <input (keyup.enter)="onKeyUp($event)" />`
+    // Event handling basic: <button (click)="onSave($event)">Save</button>`
+    // Style Binding: <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>`
+    // Class Binding: <button class="btn btn-primary" [class.active]="isActive">Save</button>`
 })
 
 export class CoursesComponent {
   title = "List of courses";
   courses;
+  isActive = true;
+  email="me@example.com";
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
-}
+  // Event binding basic
+  // onSave($event) {
+  //   console.log("button was clicked", $event);
+  // }
+
+  // Looking for enter to be edited: 
+  // onKeyUp($event){
+  //   console.log($event.target.value);
+  // }
+
+  // Template variables: 
+  // onKeyUp(email){
+  //   console.log(email);
+  // }
+  // Two-way binding
+  onKeyUp(){
+    console.log(this.email);
+  }
+} 
